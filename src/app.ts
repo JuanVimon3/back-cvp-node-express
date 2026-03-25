3a219284269a418d5194c6736d2d9470ee6d2f56
JuanVimon3 <jvillamonsalve@gmail.com>
2026-03-25 11:41:47 -0500

    implement register function

--- src/app.ts
+++ src/app.ts
@@ -45,14 +45,34 @@ const login = async (req: any, res: any) => { // Función asincrónica para mane
     
 };
 
+
+const register = async (req: any, res: any) => { // Función asincrónica para manejar el registro de usuarios
+    try {
+
+        const {nombre, contraseña, cedula, email} = req.body;
+        const [result]: any = await connection.execute('INSERT INTO Usuario (nombre, contraseña, cedula, email) VALUES (?, ?, ?, ?)', [nombre, contraseña, cedula, email]);
+
+        if(result && result.affectedRows > 0) {
+            res.status(201).json({message: 'Registro exitoso', user: {nombre, cedula, email}});  
+        } else {
+            res.status(400).json({message: 'Error al registrar el usuario'});
+        }
+    } catch (error) {
+        console.error('Error en la consulta en la base de datos:', error);
+        res.status(500).json({message: 'Error interno del servidor'});
+    }
+}
+
+
 app.get('/', (req: any, res: any) => { // Ruta raíz del servidor
     res.send(`Servidor funcionando en http://localhost:${port}`)
 })
 
 app.post('/login', login); // Ruta para manejar el login, se llama a la función login cuando se recibe una solicitud POST en /login 
 
+app.post('/register', register); // Ruta para manejar el registro de usuarios, se llama a la función register cuando se recibe una solicitud POST en /register
+
+
 app.listen(port, () => { // Iniciar el servidor en el puerto especificado, en este caso 3000
     console.log(`Servidor escuchando en http://localhost:${port}`); 
 })
-
-// Pendiente registrar usuario 
